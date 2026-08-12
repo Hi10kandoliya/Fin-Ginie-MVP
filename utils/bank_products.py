516c2ee5af7eda6366c7b51e3ae7dfb057e607f2
Hi10kandoliya <hitenkumar.kandoliya@gmail.com>
2026-08-12 15:06:36 -0500

    Add bank products data to bank_products.py

---
+++ utils/bank_products.py
@@ -0,0 +1,33 @@
+bank_products = [
+    {
+        "bank": "Chase Bank",
+        "product": "Savings Account",
+        "product_type": "Deposit",
+        "minimum_deposit": 500,
+        "monthly_fee": 0,
+        "apy": 0,
+        "official_url": "https://www.chase.com/personal/savings/savings-account",
+        "application_url": "https://secure.chase.com/web/oao/application/retail?cfgCode=010111&zipCode=&eCouponCode=#/origination/index/index",
+    },
+
+    {
+        "bank": "Chase Bank",
+        "product": "CD",
+        "product_type": "Deposit",
+        "minimum_deposit": None,
+        "monthly_fee": 0,
+        "apy_url": "https://www.chase.com/personal/savings/bank-cd#rates",
+        "official_url": "https://www.chase.com/personal/savings/bank-cd",
+        "application_url": "https://www.chase.com/personal/savings/bank-cd#get-started",
+    },
+    {
+        "bank": "DCU",
+        "product": "Savings Account",
+        "product_type": "Deposit",
+        "minimum_deposit": 0,
+        "monthly_fee": 0,
+        "apy": 5.00 %,
+        "official_url": "https://www.dcu.org/bank/savings/primary-savings.html",
+        "application_url": "https://www.dcu.org/bank/open-an-account.html?key=204689",
+    },
+]
