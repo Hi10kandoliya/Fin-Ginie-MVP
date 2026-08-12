e442b6cdf486a850600c80788f4497a99be7a72f
Hi10kandoliya <hitenkumar.kandoliya@gmail.com>
2026-08-12 14:51:39 -0500

    Add bank data for Chase Bank and DCU
    Added bank data for Chase Bank and DCU including types, descriptions, websites, and products.

---
+++ utils/bank_data.py
@@ -0,0 +1,28 @@
+banks = {
+    "Chase Bank": {
+        "bank_type": "National Bank",
+        "description": "Chase online lets you manage your Chase accounts, view statements, monitor activity, pay bills or transfer funds securely from one central place.",
+        "website": "https://www.chase.com",
+        "products": [
+            "Savings Account",
+            "Checking Account",
+            "CD",
+            "Credit Card",
+            "Home Loan",
+            "Auto Loan",
+        ],
+    },
+
+    "DCU": {
+        "bank_type": "Credit Union",
+        "description": "Enjoy no monthly fees, great rates, and digital tools with DCU checking & savings. Open a DCU account with no monthly fees, high dividends, and great member perks. Free Online Banking. Refinance Today. Save Money.",
+        "website": "https://www.dcu.org/",
+        "products": [
+            "Savings Account",
+            "Checking Account",
+            "Auto Loan",
+            "Personal Loan",
+            "Home Loan",
+        ],
+    },
+}
